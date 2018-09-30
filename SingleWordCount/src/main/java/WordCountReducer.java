4dfa191e02791820867b5cc7fcc5cef5d94209c2
gurupad93 <43187717+gurupad93@users.noreply.github.com>
2018-09-30 14:22:22 -0400

    Updated comments

--- SingleWordCount/src/main/java/WordCountReducer.java
+++ SingleWordCount/src/main/java/WordCountReducer.java
@@ -6,7 +6,9 @@ import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
-//Reducer class
+/**
+* Reducer class
+*/
 public class WordCountReducer extends Reducer<Text, IntWritable, Text, IntWritable>{
 
 	@Override
