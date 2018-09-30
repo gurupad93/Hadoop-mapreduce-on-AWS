acdd475a7fcd74499d4afcd77d11ba60e6b37fa4
gurupad93 <43187717+gurupad93@users.noreply.github.com>
2018-09-30 15:14:16 -0400

    Update WordCountMapper.java

--- SingleWordCount/src/main/java/WordCountMapper.java
+++ SingleWordCount/src/main/java/WordCountMapper.java
@@ -13,8 +13,11 @@ import org.apache.hadoop.mapreduce.Mapper;
 */
 public class WordCountMapper extends Mapper<LongWritable, Text, Text, IntWritable>{
 	public void map(LongWritable key, Text value, Context con) throws IOException, InterruptedException {
+		//Get All lines from the input files
 		String allLines = value.toString();
+		//Extract all the words
 		String[] allWords=allLines.split(" ");
+		//Map each of the extracted words.
 		for(String word: allWords ) {
 			Text outputKeyWord = new Text(word.trim());
 			IntWritable outputValue = new IntWritable(1);
